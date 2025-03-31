e904e530da570edfaa03e6c22628c9c9c25b098a
Alex Hollanda <alex.itera360@hotmail.com>
2025-03-30 21:12:20 -0300

    Feat: React Inicial

---
+++ sgosat.app/src/paginas/Home/Home.js
@@ -0,0 +1,9 @@
+import style from "./Home.module.css";
+
+export function Home() {
+    return (
+        <div className={style.conteudo}>
+            <h3>Bienvenido a SGOSAT</h3>
+        </div>
+    );
+}
